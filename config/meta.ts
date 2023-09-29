f5ff0920e447310f149d62dcf98e28acecca2513
Samuel Braun <office.samigo.a@gmail.com>
2023-09-29 20:13:28 +0200

    Please be fixed OG

--- config/meta.ts
+++ config/meta.ts
@@ -11,48 +11,6 @@ const meta: NuxtOptionsHead = {
       name: 'description',
       content: description,
     },
-    {
-      hid: 'title',
-      name: 'title',
-      content: 'Song Timer',
-    },
-    {
-      hid: 'description',
-      name: 'description',
-      content:
-        "A web app to align your song's beat to your game's beat. This app is still new and was tested primarily with Beat Saber.",
-    },
-    {
-      hid: 'og:description',
-      property: 'og:description',
-      content:
-        "A web app to align your song's beat to your game's beat. This app is still new and was tested primarily with Beat Saber.",
-    },
-    {
-      hid: 'og:image',
-      property: 'og:image',
-      content: 'https://song-timer.webry.com/meta/cover.png',
-    },
-    {
-      hid: 'og:title',
-      property: 'og:title',
-      content: 'Song Timer',
-    },
-    {
-      hid: 'og:type',
-      property: 'og:type',
-      content: 'website',
-    },
-    {
-      hid: 'og:url',
-      property: 'og:url',
-      content: 'https://song-timer.webry.com/',
-    },
-    {
-      hid: 'og:site_name',
-      property: 'og:site_name',
-      content: 'Song Timer',
-    },
   ],
   link: [
     { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -7,6 +7,54 @@ export default defineNuxtConfig({
   ssr: false,
   // https://v3.nuxtjs.org/docs/directory-structure/nuxt.config#meta
   meta,
+  app: {
+    head: {
+      meta: [
+        {
+          hid: 'title',
+          name: 'title',
+          content: 'Song Timer',
+        },
+        {
+          hid: 'description',
+          name: 'description',
+          content:
+            "A web app to align your song's beat to your game's beat. This app is still new and was tested primarily with Beat Saber.",
+        },
+        {
+          hid: 'og:description',
+          property: 'og:description',
+          content:
+            "A web app to align your song's beat to your game's beat. This app is still new and was tested primarily with Beat Saber.",
+        },
+        {
+          hid: 'og:image',
+          property: 'og:image',
+          content: 'https://song-timer.webry.com/meta/cover.png',
+        },
+        {
+          hid: 'og:title',
+          property: 'og:title',
+          content: 'Song Timer',
+        },
+        {
+          hid: 'og:type',
+          property: 'og:type',
+          content: 'website',
+        },
+        {
+          hid: 'og:url',
+          property: 'og:url',
+          content: 'https://song-timer.webry.com/',
+        },
+        {
+          hid: 'og:site_name',
+          property: 'og:site_name',
+          content: 'Song Timer',
+        },
+      ],
+    },
+  },
   // https://v3.nuxtjs.org/docs/directory-structure/nuxt.config#publicruntimeconfig
   publicRuntimeConfig,
   // https://v3.nuxtjs.org/docs/directory-structure/nuxt.config#modules
